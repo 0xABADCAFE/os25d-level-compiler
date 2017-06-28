756dab4be16ecbcf7dc6047b02e3f2fdebca6c24
Hamza <karlchurchill@gmail.com>
2017-06-28 01:46:51 +0100

    Reworked export file to include proper header with file ident, version number and a Hunk Index table at the start so that the runtime can know exactly how many hunks are present, what type each hunk is and at what seek position it lies.

--- level_compiler/src/domain/export.php
+++ level_compiler/src/domain/export.php
@@ -63,13 +63,31 @@ trait TBinaryExportable {
 }
 
 /**
- * BinaryExportFile class. Used to create the final level data to be used by the engine.
+ * BinaryExportFile class. Used to create the final level data to be loaded and used by the engine. The file begins with a header,
+ * then an index table of all the objects contained, and finally the objects themselves. The index table allows the runtime to quickly
+ * seek() to an object for loading.
+ *
+ * File Format:
+ *
+ * [  0 : File ID        : uint8[8]             ] File identifier
+ * [  8 : Version        : uint32               ] File version
+ * [ 12 : Hunk Count     : uint32               ] Number of Hunks in file
+ * [ 16 : Hunk Index [0] : { uint8[4], uint32 } ] Hunk ID / Seek offset pair for first Hunk
+ * [ ...                                   ]
+ * [ XX : Hunk Index [N] : { uint8[4], uint32 } ] Hunk ID / Seek offset pair for final Hunk
+ * [ YY : Hunk [0] ]                              First Hunk
+ * [ ........ ]
+ * [ ZZ : Hunk [N] ]                              Last Hunk
  */
 class BinaryExportFile {
 
   const
-    ALIGN_SIZE = 4,
-    IDENT_SIZE = 4
+    VERSION      = 1,
+    ALIGN_SIZE   = 4,
+    IDENT_SIZE   = 4,
+    LOOKUP_SIZE  = 8,
+    FILE_ID      = 'OS25DLvl',
+    HEADER_SIZE  = 16
   ;
 
   use TBinaryExportable;
@@ -81,27 +99,27 @@ class BinaryExportFile {
   }
 
   public function open(string $sFile) : self {
-    if (!$this->rFile = fopen($sFile, 'w')) {
+    if (
+      (!$this->rFile = fopen($sFile, 'w'))   ||
+      (!fwrite($this->rFile, self::FILE_ID)) ||
+      (!fwrite($this->rFile, $this->intToU32BE(self::VERSION)))
+    ) {
       throw new IOWriteException($sFile);
     }
-
     return $this;
   }
 
   /**
-   * Writes an encapsulated IBinaryExportable record to the output file. The format of the payload at offset N is as follows:
+   * Exports an encapsulated IBinaryExportable record to the hunk array for writing. The encoded payload is as follows
    *
-   *   N + 0       : Identifier (char[8])
-   *   N + 8       : Length     (uint32 big endian)
-   *   N + 12      : crc32      (uint32 big endian)
-   *   N + 16      : data       (uint8[X])
-   *   N + 16 + X  : Zero pad until next ALIGN_SIZE byte aligned offset
+   * [  0      : Identifier : uint8[4] ] Hunk identifier
+   * [  8      : Length     : uint32   ] Hunk data length (not including header and padding)
+   * [ 12      : crc32      : uint32   ] Hunk data CRC32
+   * [ 16      : data       : uint8[X] ] Hunk data
+   * [ 16 + X  : Zero pad   : uint8[N] ] Zero pad until next ALIGN_SIZE byte aligned offset, may be zero
    */
 
   public function export(IBinaryExportable $oExportable) : self {
-    if (!$this->rFile) {
-      throw new IOWriteException();
-    }
     $sIdent   = $this->padToBoundary(substr($oExportable->getBinaryIdent(), 0, self::IDENT_SIZE), self::IDENT_SIZE);
     $sBinary  = $oExportable->getBinaryData();
     $iLength  = strlen($sBinary);
@@ -109,19 +127,49 @@ class BinaryExportFile {
     $sBinary  = $this->padToBoundary($sBinary, self::ALIGN_SIZE);
     $sPayload = $sIdent . $this->arrayIntToU32BE([$iLength, $iCheck]) . $sBinary;
 
-    if (!fwrite($this->rFile, $sPayload)) {
-      throw new IOWriteException();
-    }
+    $this->aHunks[] = (object)[
+      'id'   => $sIdent,
+      'data' => $sPayload
+    ];
 
     return $this;
   }
 
   public function close() {
     if ($this->rFile) {
+      $this->writeIndex();
+      $this->writeHunks();
       fclose($this->rFile);
       $this->rFile = null;
     }
   }
 
-  private $rFile = null;
+  private function writeIndex() {
+
+    if (!fwrite($this->rFile, $this->intToU32BE(count($this->aHunks)))) {
+      throw new IOWriteException();
+    }
+    $iSeek = self::HEADER_SIZE + (count($this->aHunks) * self::LOOKUP_SIZE);
+    foreach ($this->aHunks as $oHunk) {
+      $sOffset = $this->intToU32BE($iSeek);
+      if (!fwrite($this->rFile, $oHunk->id . $sOffset)) {
+        throw new IOWriteException();
+      }
+      $iSeek += strlen($oHunk->data);
+    }
+
+  }
+
+  private function writeHunks() {
+    foreach ($this->aHunks as $oHunk) {
+      if (!fwrite($this->rFile, $oHunk->data)) {
+        throw new IOWriteException();
+      }
+    }
+  }
+
+  private
+    $rFile   = null,
+    $aHunks  = []
+  ;
 }
